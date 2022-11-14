9bd536f57f42a308c3f6a115ba1a4d4e8274958f
WillRos06 <82073842+WillRos06@users.noreply.github.com>
2022-11-14 22:20:49 +0000

    edit

--- producer.js
+++ producer.js
@@ -1,6 +1,6 @@
 const amqp = require("amqplib");
 
-const rabbitSettings = {
+const rabbitSettings = [{
     protocol: 'amqp',
     hostname: 'localhost',
     port: 5672,
@@ -8,8 +8,14 @@ const rabbitSettings = {
     password: 'guest',
     vhost: '/',
     authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
-
-}
+},{
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5673,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']}]
 
 connect();
 
@@ -25,25 +31,29 @@ async function connect(){
         {   "name":"Wiliam Del Rosario", "Materia":"Algoritmo Paralelos"},
     ]
 
-    try{
+    for(i=0;i<rabbitSettings.length;i++){
+        try{
 
-        const conn = await amqp.connect(rabbitSettings);
-        console.log("Connection Created...");
-
-        const channel = await conn.createChannel();
-        console.log("Channel Created...");
-
-        const res = await channel.assertQueue(queue);
-        console.log("Queue Created...");
-
-        for(let msg in msgs){
-            await channel.sendToQueue(queue, Buffer.from(JSON.stringify(msgs[msg])));
-            console.log('Mensaje enviado a la cola ${queue}');
+            const conn = await amqp.connect(rabbitSettings[i]);
+            console.log("Connection Created...");
+    
+            const channel = await conn.createChannel();
+            console.log("Channel Created...");
+    
+            const res = await channel.assertQueue(queue);
+            console.log("Queue Created...");
+    
+            for(let msg in msgs){
+                await channel.sendToQueue(queue, Buffer.from(JSON.stringify(msgs[msg])));
+                console.log('Mensaje enviado a la cola ${queue}');
+            }
+    
+    
+            break;
+        } catch(err){
+            console.error(`Error -> ${err}`);
         }
-
-
-
-    } catch(err){
-        console.error(`Error -> ${err}`);
     }
+
+
 }
