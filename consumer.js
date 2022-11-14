a0c306200ed46a27e009870accec0ecbd88de317
WillRos06 <82073842+WillRos06@users.noreply.github.com>
2022-11-14 17:48:36 -0400

    Add files via upload

--- consumer.js
+++ consumer.js
@@ -18,37 +18,42 @@ async function connect(){
     const queue = "Tarea";
     const Materia = "UTESA"
 
+    for(i=0;i<rabbitSettings.length;i++){
+        try{
 
-    try{
+            const conn = await amqp.connect(rabbitSettings);
+            console.log("Conexion creada...");
+    
+            const channel = await conn.createChannel();
+            console.log("Canal creado...");
+    
+            const res = await channel.assertQueue(queue);
+            console.log("Queue creado...");
+    
+            console.log('Esperando por mensajes from ${Materia}');
+            channel.consume(queue, message => {
+                let Tarea = JSON.parse(message.content.toString());
+                console.log('Tarea recibido ${Tarea.name}');
+                console.log(Tarea);
+    
+                if(Tarea.Materia == Materia){
+                    channel.ack(message);
+                    console.log("Mensaje borrado de la cola... \n");
+    
+                } else{
+                    console.log("Ese mensaje no es para mi, por lo tanto, no lo borrare.");
+                }
+    
+    
+            })
+    
+            break;
+        } catch(err){
+            console.error('Error -> $(err)');
+        }
 
-        const conn = await amqp.connect(rabbitSettings);
-        console.log("Conexion creada...");
-
-        const channel = await conn.createChannel();
-        console.log("Canal creado...");
-
-        const res = await channel.assertQueue(queue);
-        console.log("Queue creado...");
-
-        console.log('Esperando por mensajes from ${Materia}');
-        channel.consume(queue, message => {
-            let Tarea = JSON.parse(message.content.toString());
-            console.log('Tarea recibido ${Tarea.name}');
-            console.log(Tarea);
-
-            if(Tarea.Materia == Materia){
-                channel.ack(message);
-                console.log("Mensaje borrado de la cola... \n");
-
-            } else{
-                console.log("Ese mensaje no es para mi, por lo tanto, no lo borrare.");
-            }
-
-
-        })
-
-
-    } catch(err){
-        console.error('Error -> $(err)');
     }
+
+
+
 }
